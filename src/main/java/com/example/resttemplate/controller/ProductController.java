e3ee89c894c14ac2a6e6385d4b3f95b390e404b6
Bukola7O <99530747+Bukola7O@users.noreply.github.com>
2024-01-09 08:52:20 +0100

    Initial commit

---
+++ src/main/java/com/example/resttemplate/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.example.resttemplate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping (value = "/template/products")
+    public String getProductList() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        return restTemplate.exchange(
+                " https://dummyjson.com/products/1", HttpMethod.GET, entity, String.class).getBody();
+    }
+}
---
+++ src/main/java/com/example/resttemplate/model/Product.java
@@ -0,0 +1,14 @@
+package com.example.resttemplate.model;
+
+public class Product {
+ private String title;
+ private String description;
+ private Long price;
+ private String discountedPercentage;
+ private String stock;
+ private String brand;
+ private String category;
+ private String thumbnail;
+ private String images;
+
+}
